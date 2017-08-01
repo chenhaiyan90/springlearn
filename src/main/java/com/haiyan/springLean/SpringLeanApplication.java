74743659183de6465f1aa7a4894894dc7dc0d398
chenhaiyan90 <595316059@qq.com>
2017-08-01 09:52:50 +0800

    init springboot project

---
+++ src/main/java/com/haiyan/springLean/SpringLeanApplication.java
@@ -0,0 +1,12 @@
+package com.haiyan.springLean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLeanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/haiyan/springLean/SpringLeanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haiyan.springLean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLeanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
